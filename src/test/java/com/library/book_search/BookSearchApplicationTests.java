c0052115068776700e0055fc6f0143489735cee6
ljh468 <ljh468@naver.com>
2025-01-10 04:16:06 +0900

    initial commit

---
+++ src/test/java/com/library/book_search/BookSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.book_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
