a21b257c3aec4c532c30fd9f1911f2cfcc49a4fb
ljh468 <ljh468@naver.com>
2025-01-16 02:52:32 +0900

    [BS-8] 문서화 (swagger 적용)

--- search-api/src/main/java/com/library/controller/BookController.java
+++ search-api/src/main/java/com/library/controller/BookController.java
@@ -1,10 +1,18 @@
 package com.library.controller;
 
 import com.library.controller.request.SearchRequest;
+import com.library.controller.response.ErrorResponse;
 import com.library.controller.response.PageResult;
 import com.library.controller.response.SearchResponse;
 import com.library.controller.response.StatResponse;
 import com.library.service.BookApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,17 +27,26 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/v1/books") // 버저닝을 통한 무중단 배포
+@RequestMapping("/v1/books") // 버저닝을 통한 무중단 배포에 사용
+@Tag(name = "book", description = "book api")
 public class BookController {
 
   private final BookApplicationService bookApplicationService;
 
+  @Operation(summary = "search API", description = "도서 검색결과 제공", tags = {"book"})
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PageResult.class))),
+      @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
   @GetMapping
   public PageResult<SearchResponse> search(@Valid SearchRequest request) {
     log.info("[BookController] search request={}", request);
     return bookApplicationService.search(request.query(), request.page(), request.size());
   }
 
+  @Operation(summary = "stats API", description = "도서 검색 통계 제공", tags = {"book"})
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = StatResponse.class))),
+      @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
   @GetMapping("/stats")
   public StatResponse findQueryStats(@RequestParam(name = "query") String query,
                                      @RequestParam(name = "date") LocalDate date) {
@@ -37,6 +54,10 @@ public class BookController {
     return bookApplicationService.findQueryCount(query, date);
   }
 
+  @Operation(summary = "stats ranking API", description = "상위5개 도서 검색 통계 제공", tags = {"book"})
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = StatResponse.class)))),
+      @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
   @GetMapping("/stats/ranking")
   public List<StatResponse> findTop5State() {
     log.info("[BookController] find top 5 stats");
--- search-api/src/main/java/com/library/controller/response/StatResponse.java
+++ search-api/src/main/java/com/library/controller/response/StatResponse.java
@@ -1,3 +1,10 @@
 package com.library.controller.response;
 
-public record StatResponse(String query, long count) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "검색 통계 응답")
+public record StatResponse(
+    @Schema(description = "쿼리", example = "HTTP")
+    String query,
+    @Schema(description = "검색횟수", example = "10")
+    long count) {}
